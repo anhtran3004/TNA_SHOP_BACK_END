c3314b8b693ce4c562892840e0b07414fb1e9b56
anhtran <trananh30044003@gmail.com>
2023-04-24 01:20:44 +0700

    update api insert order

--- order.js
+++ order.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const formatDate = require('./formatDate');
 router.post('/insert-order', (req, res) =>{
     const {order_input} = req.body; 
-    let sql = 'INSERT INTO orders (name, email, address, phone, ship_name, created_date, method_delivery, user_id, shipping_fee) VALUES';
+    let sql = 'INSERT INTO orders (name, email, address, phone, ship_name, created_date, method_delivery, user_id, shipping_fee,total_price) VALUES';
     if(order_input){
-        sql+= ` ("${order_input.name}", "${order_input.email}","${order_input.address}", "${order_input.phone}", "${order_input.ship_name}", "${formatDate()}","${order_input.method_delivery}", ${order_input.user_id}, ${order_input.shipping_fee})`;
+        sql+= ` ("${order_input.name}", "${order_input.email}","${order_input.address}", "${order_input.phone}", "${order_input.ship_name}", "${formatDate()}","${order_input.method_delivery}", ${order_input.user_id}, ${order_input.shipping_fee}, ${order_input.total_price})`;
     }
     console.log(sql);   
     // execute query
@@ -33,6 +33,23 @@ router.post('/insert-order-product', (req, res) =>{
         }else{
             // res.send(results);
             res.status(200).send({code: 200, message:'success!'})
+        }
+    }) 
+})
+router.post('/', (req, res) =>{
+    const {status} = req.body; 
+    let sql = 'SELECT * FROM orders WHERE 1=1';
+    if(status && status.length > 0){
+        sql += ` AND status IN (${status.join()})`;
+    }
+    console.log(sql);   
+    // execute query
+    db.query(sql, (error,  results) => {
+        if(error){
+            res.status(500).send({error: 'Error fetching orders form database'});
+        }else{
+            // res.send(results);
+            res.status(200).send({code: 200, message:'success!', data: results})
         }
     }) 
 })
