d6942a732cf67cfa0925f8d8e23ce1dd593d2441
Anh Tran <trananh30044003@gmail.com>
2023-04-25 17:20:24 +0700

    update api mange list favorite product

--- favorite.js
+++ favorite.js
@@ -6,33 +6,47 @@ const router = express.Router()
 router.post('/insert-favorite',authenticate('user'), (req, res)=>{
     const {product_id} = req.body;
     const user_ID = req.user.id;
-    console.log(user_ID);
-    let sql = `INSERT INTO favorites (user_id, product_id) VALUES`;
-    if(product_id){
-        sql += `(${user_ID}, ${product_id})`;
-    }
+    
+    // let sql = `INSERT INTO favorites (user_id, product_id) VALUES`;
+    // if(product_id){
+    //     sql += `(${user_ID}, ${product_id})`;
+    // }
+    console.log(user_ID)
+    let sql = `INSERT INTO favorites (user_id, product_id) 
+             SELECT ${user_ID}, ${product_id}
+             WHERE NOT EXISTS (
+               SELECT 1 FROM favorites 
+               WHERE user_id = ${user_ID} AND product_id = ${product_id}
+             )`;
     console.log(sql);
     db.query(sql, (error, results) =>{
-        if(error){
-            res.status(500).send({code: 500, message:"Error insert color"});
-        }else{
-            res.status(200).send({code: 200, message:"insert color sucess"});
-        }
+        if (error) {
+            res.status(500).send({ code: 500, message: "Error inserting favorite" });
+          } else {
+            if (results.affectedRows === 0) {
+              res.status(200).send({ code: 2000, message: "Product already exists in favorites" });
+            } else {
+              res.status(200).send({ code: 200, message: "Inserted favorite successfully" });
+            }
+          }
     })
 });
 router.post('/delete-favorite/:id',authenticate('user'), (req, res) =>{
-    const id = req.body.id;
-    let sql = `DELETE FROM favorites WHERE id=` + id
-    db.query(sql, [ids], (error, results) => {
+    const id = req.params.id;
+    let sql = `DELETE FROM favorites WHERE product_id=` + id
+    db.query(sql, (error, results) => {
         if(error){
-            res.status(500).send({code: 500, message:'Error deleting colors'});
+            res.status(500).send({code: 500, message:'Error deleting favorites'});
+            console.log(sql);
         }else{
-            res.send({code: 200, message: `Deleted ${results.affectedRows} colors`});
+            res.send({code: 200, message: `Deleted ${results.affectedRows} favorites`});
+            console.log(sql);
         }
     })
 })
 router.post('/',authenticate('user'), (req, res) =>{
-    let sql = `SELECT * FROM favorites`;
+    const user_ID = req.user.id;
+    let sql = `SELECT * FROM favorites WHERE user_id=`+ user_ID;
     console.log(sql);
     db.query(sql, (error, results) => {
         if(error){
