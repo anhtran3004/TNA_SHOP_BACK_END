a0304e3a87b1d462768800dc8dc28991c459d089
anhtran <trananh30044003@gmail.com>
2023-05-02 15:31:35 +0700

    add authenticate function

--- jwt.js
+++ jwt.js
@@ -15,7 +15,22 @@ function authenticate(role) {
     });
   };
 }
+function authenticates(roles) {
+  return (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).send('Authorization header missing');
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) return res.status(401).send('Invalid token');
+      console.log(decoded);
+      if (!roles.includes(decoded.role)) return res.status(403).send('Forbidden');
+      req.user = decoded;
+      next();
+    });
+  };
+}
 
 module.exports = {
-  authenticate
+  authenticate,
+  authenticates
 };
