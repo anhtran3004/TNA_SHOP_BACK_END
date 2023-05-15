b348dc85df00c55d9f93ccf16d7a36e92f9a6496
Anh Tran <trananh30044003@gmail.com>
2023-05-15 18:09:04 +0700

    update api get list category

--- category.js
+++ category.js
@@ -17,6 +17,29 @@ router.post('/', (req, res) =>{
         }
     })
 })
+router.post('/get-category-with-filter', (req, res) =>{
+    // const {category_input} = req.body.product_input;
+    let sql = 'SELECT * FROM categories WHERE 1=1 ';
+    const {filter} = req.body;
+    const dbParams = [];
+    if(filter){
+        // add search condition
+        if(filter.search){
+            const searchValue = `%${filter.search}%`;
+            sql += ` AND (categoryName LIKE ?)`;
+            dbParams.push(searchValue);
+        }
+    }
+    db.query(sql, dbParams, (error, results) =>{
+        if(error){
+            res.status(500).send({code: 500, message:"error get categories"})
+            console.log(sql);
+        }else{
+            res.status(200).send({code: 200, message:"success", data: results});
+            console.log(sql);
+        }
+    })
+})
 router.post('/insert-category', authenticates(['admin', 'employee']) , (req, res)=>{
     const {category_input} = req.body;
     if(!category_input){
@@ -64,6 +87,19 @@ router.put('/delete-category' , authenticates(['admin', 'employee']), (req, res)
     
     console.log(sql);
     db.query(sql,[status, ids], (error, results) =>{
+        if(error){
+            res.status(500).send({code: 500, message:"error delete category"})
+        }else{
+            res.status(200).send({code: 200, message:"insert success!"})
+        }
+    })
+})
+router.post('/delete-categories/:id' , authenticates(['admin', 'employee']), (req, res) =>{
+    const id = req.params.id;
+    const sql = 'DELETE FROM categories WHERE id = ' + id;
+    
+    console.log(sql);
+    db.query(sql, (error, results) =>{
         if(error){
             res.status(500).send({code: 500, message:"error delete category"})
         }else{
